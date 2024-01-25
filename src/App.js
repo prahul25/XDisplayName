8031f7f302474f954a73410cf4e301db508ca48c
prahul25 <palrahulkumar2002@gmail.com>
2024-01-25 12:52:15 +0530

    Try to resolve some test cases

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
   const [fullName, setFullName] = useState("");
   function handleSubmit(event) {
-    event.preventDefault()
-    setFullName("Full Name: " + firstName + " " + lastName);
+    event.preventDefault();
+    setFullName(`Full Name: ${first} ${last}`);
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
       <h1>Full Name Display</h1>
-
-      <label>First Name:</label>
-      <input
-        value={firstName}
-        type="text"
-        onChange={(e) => setFirstName(e.target.value)}
-        required
-      />
-      <br />
-      <label>Last Name:</label>
-      <input
-        value={lastName}
-        type="text"
-        onChange={(e) => setLastName(e.target.value)}
-        required
-      />
-      <br />
-
-      <button>Submit</button>
-      <p>{fullName}</p>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+        First Name:
+        <input
+          value={first}
+          type="text"
+          onChange={(e) => setFirst(e.target.value)}
+          required
+        />
+        </div>
+        <div>
+        Last Name:
+        <input
+          value={last}
+          type="text"
+          onChange={(e) => setLast(e.target.value)}
+          required
+        />
+        </div>
+        <div>
+        <button type="submit">Submit</button>
+        </div>
+        </div>
+        <p>{fullName}</p>
+      </form>
+    </div>
   );
 }
 
